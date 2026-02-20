530c4a6414d1b7f00c1c4c66c6723800124a7e1f
museng3n <m2jengnnn@gmail.com>
2026-02-20 11:18:48 +0300

    GHL-53: read token from URL param

---
+++ app/page.tsx
@@ -0,0 +1,20 @@
+"use client"
+
+import { useEffect } from "react"
+import { useRouter } from "next/navigation"
+
+export default function Home() {
+  const router = useRouter()
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search)
+    const urlToken = urlParams.get('token')
+    if (urlToken) {
+      localStorage.setItem('authToken', urlToken)
+      window.history.replaceState({}, '', window.location.pathname)
+    }
+    router.replace("/campaigns/create")
+  }, [router])
+
+  return null
+}
